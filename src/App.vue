0cd531ac3f32e5da60769df1cfebe215e51b4374
yyunon <yonsel.yuksel@gmail.com>
2023-07-24 17:52:31 +0200

    Build project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <title>"This is title"</title>
+  <HelloWorld msg="Coming soon..."/>
 </template>
 
 <script>
