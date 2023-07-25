4c60d2c6628f5ffd79d9a2c58080e7b8c619db61
yyunon <yonsel.yuksel@gmail.com>
2023-07-25 10:58:42 +0200

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/yukselyonsel/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   transpileDependencies: true
 })
