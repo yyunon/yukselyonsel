6d5c6900c5a3852710afef1ba9c10cbba76729da
yyunon <yonsel.yuksel@gmail.com>
2023-07-25 11:00:42 +0200

    Change repo name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/yyunon.github.io/" : "/",
   transpileDependencies: true
 })
