7808cad2c731c632db8b31eae03040115ce5e5ab
yyunon <yonsel.yuksel@gmail.com>
2023-09-08 18:20:53 +0200

    Fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,23 @@
-import axios from 'axios'
+import axios from 'axios';
+
 
 const apiClient = axios.create({
   withCredentials: false,
   headers: {
+    'Access-Control-Allow-Origin': '*',
     Accept: 'application/json'
   },
   validateStatus: function() {
     return true;
-  }
+  },
 });
 
 const events = {
   async get(path) {
-    return apiClient.get("https://35.208.253.242/api" + path, {timeout: 1000})
+    return apiClient.get("http://35.208.253.242:80/api" + path, {timeout: 1000})
   },
   async post(path, body) {
-    return apiClient.post("https://35.208.253.242/api" + path, body, {timeout: 1000})
+    return apiClient.post("http://35.208.253.242:80/api" + path, body, {timeout: 1000})
   }
 }
 
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,4 @@ module.exports = defineConfig({
   publicPath: "/",
   transpileDependencies: true,
   runtimeCompiler: true,
-  devServer: {
-    proxy: 'http://35.208.253.242:80/api',
-    https: true,
-  }
 })
